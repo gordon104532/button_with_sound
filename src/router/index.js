5d1d822da5f1f660eee75995397386fd403eb029
gordon104532 <gordon1045321@gmail.com>
2023-12-25 00:26:09 +0800

    feat: about page turn into share page

--- src/router/index.js
+++ src/router/index.js
@@ -8,12 +8,12 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/share',
+    name: 'share',
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (share.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "share" */ '../views/ShareView.vue')
   }
 ]
 
--- src/views/ShareView.vue
+++ src/views/ShareView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="about">
-    <h1>這一頁要放甚麼?</h1>
+  <div class="share">
+    <h1>這一頁要放甚麼，分享一些好料</h1>
     <!-- 波普貓 SoundCloud -->
     <iframe width="100%" height="600" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/228593349&color=%23745c6c&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
     <div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;">
@@ -8,5 +8,20 @@
       <a href="https://soundcloud.com/engblue/lost-stars-keira-knightleycover" title="Lost Stars - Keira Knightley(cover)" target="_blank" style="color: #cccccc; text-decoration: none;">Lost Stars - Keira Knightley(cover)</a>
     </div>
 
+    <br>
+    <br>
+
+    <div>
+      <h3>白澤大哥哥的頻道</h3>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/F8UnNgTxyN8?si=dk-FbKf1GRXIlV7Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+
+    <br>
+    <br>
+
+    <div>
+      <h3>威廉大哥哥的頻道</h3>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/4iDU1GN3ssc?si=p0nzBa_TrA9qYSm0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
   </div>
 </template>
