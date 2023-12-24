89dae235201564a1677db93b56d725a26ad67f3c
gordon104532 <gordon1045321@gmail.com>
2023-12-24 23:58:27 +0800

    feat: 更新歌單

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
     <h1>這一頁要放甚麼?</h1>
+    <!-- 波普貓 SoundCloud -->
+    <iframe width="100%" height="600" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/228593349&color=%23745c6c&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+    <div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;">
+      <a href="https://soundcloud.com/engblue" title="EngBlue波普貓在英格藍" target="_blank" style="color: #cccccc; text-decoration: none;">EngBlue波普貓在英格藍</a>
+      <a href="https://soundcloud.com/engblue/lost-stars-keira-knightleycover" title="Lost Stars - Keira Knightley(cover)" target="_blank" style="color: #cccccc; text-decoration: none;">Lost Stars - Keira Knightley(cover)</a>
+    </div>
+
   </div>
 </template>
